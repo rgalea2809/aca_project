fe8853d5f26b6205c83723730b50a7e30bcea949
Rodrigo Mejia Galea <54407065+rgalea2809@users.noreply.github.com>
2023-05-17 17:00:27 -0600

    :construction: WIP: Adds geoportal placeholder

---
+++ src/modules/geoportal/GeoPortal.jsx
@@ -0,0 +1,8 @@
+import { useState, useEffect, useRef } from "react";
+import Globe from "react-globe.gl";
+
+function GeoPortal({}) {
+  return <div className="geoportal"></div>;
+}
+
+export default GeoPortal;
