27fc8c85d41b2abcfa4039d6d4635526f8cf5137
Rodrigo Mejia Galea <54407065+rgalea2809@users.noreply.github.com>
2023-05-17 18:21:15 -0600

    :lipstick: Improve home UI

--- src/modules/home/home.jsx
+++ src/modules/home/home.jsx
@@ -1,15 +1,59 @@
+import { useState } from "react";
+import "./home.css";
 import Globe from "../globe/Globe";
+import PropTypes from "prop-types";
+import Tabs from "@mui/material/Tabs";
+import Tab from "@mui/material/Tab";
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
+
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box sx={{ p: 3 }}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
 
 export default function Home() {
+  const [currentTab, setCurrentTab] = useState(0);
+
+  const handleTabChange = (event, newValue) => {
+    setCurrentTab(newValue);
+  };
+
   return (
     <div className="block">
-      <div className="block ">
+      <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
+        <Tabs
+          value={currentTab}
+          onChange={handleTabChange}
+          aria-label="basic tabs example"
+        >
+          <Tab label="GeoPortal" />
+          <Tab label="Vista 3D" />
+        </Tabs>
+      </Box>
+      <TabPanel value={currentTab} index={0}>
         <h1>GeoPortal view</h1>
-      </div>
-
-      <div className="block">
-        <Globe />
-      </div>
+      </TabPanel>
+      <TabPanel value={currentTab} index={1}>
+        <div className="block">
+          <Globe />
+        </div>
+      </TabPanel>
     </div>
   );
 }
