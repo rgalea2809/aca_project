4f63bdf292115fb7b1d1506dd3c34be90b849201
Rodrigo Mejia Galea <54407065+rgalea2809@users.noreply.github.com>
2023-06-08 15:43:08 -0600

    :construction: WIP: Add new map container view

--- src/modules/ol_map/ol_map.jsx
+++ src/modules/ol_map/ol_map.jsx
@@ -1,126 +1,51 @@
-// react
-import React, { useState, useEffect, useRef } from "react";
-import "./ol_map.css";
+import React, { useState } from "react";
+import Map from "./components/map_container/map_container";
+import Layers from "./components/layers/layers";
+import TileLayer from "./components/layers/tile_layer";
+import VectorLayer from "./components/layers/vector_layer";
+import { Circle as CircleStyle, Fill, Stroke, Style } from "ol/style";
+import { osm, vector } from "./Source";
+import { fromLonLat, get } from "ol/proj";
+import GeoJSON from "ol/format/GeoJSON";
+import Controls from "./components/controls/controls";
+import FullScreenControl from "./components/controls/full_screen_control";
 
-// openlayers
-import Map from "ol/Map";
-import View from "ol/View";
-import TileLayer from "ol/layer/Tile";
-import VectorLayer from "ol/layer/Vector";
-import VectorSource from "ol/source/Vector";
-import XYZ from "ol/source/XYZ";
-import { transform } from "ol/proj";
-import { toStringXY } from "ol/coordinate";
-import { GeoJSON } from "ol/format";
+let styles = {
+  MultiPolygon: new Style({
+    stroke: new Stroke({
+      color: "blue",
+      width: 1,
+    }),
+    fill: new Fill({
+      color: "rgba(0, 0, 255, 0.1)",
+    }),
+  }),
+};
 
 export default function OlMap({ features, selectedGeoJsonLayer }) {
-  // set intial state
-  const [map, setMap] = useState();
-  const [featuresLayer, setFeaturesLayer] = useState();
-  const [selectedCoord, setSelectedCoord] = useState();
+  const [center, setCenter] = useState([-94.9065, 38.9884]);
+  const [zoom, setZoom] = useState(9);
 
-  // pull refs
-  const mapElement = useRef();
-
-  // create state ref that can be accessed in OpenLayers onclick callback function
-  //  https://stackoverflow.com/a/60643670
-  const mapRef = useRef();
-  mapRef.current = map;
-
-  // initialize map on first render - logic formerly put into componentDidMount
-  useEffect(() => {
-    // create and add vector source layer
-    const initalFeaturesLayer = new VectorLayer({
-      source: new VectorSource(),
-    });
-
-    // create map
-    const initialMap = new Map({
-      target: mapElement.current,
-      layers: [
-        // USGS Topo
-        /*
-        new TileLayer({
-          source: new XYZ({
-            url: "https://basemap.nationalmap.gov/arcgis/rest/services/USGSTopo/MapServer/tile/{z}/{y}/{x}",
-          }),
-        }),
-        */
-
-        // Google Maps Terrain
-        new TileLayer({
-          source: new XYZ({
-            url: "http://mt0.google.com/vt/lyrs=p&hl=en&x={x}&y={y}&z={z}",
-          }),
-        }),
-
-        initalFeaturesLayer,
-      ],
-      view: new View({
-        projection: "EPSG:3857",
-        center: [0, 0],
-        zoom: 2,
-      }),
-      controls: [],
-    });
-
-    // set map onclick handler
-    initialMap.on("click", handleMapClick);
-
-    // save map and vector layer references to state
-    setMap(initialMap);
-    setFeaturesLayer(initalFeaturesLayer);
-  }, []);
-
-  // update map if features prop changes - logic formerly put into componentDidUpdate
-  useEffect(() => {
-    if (
-      selectedGeoJsonLayer != null &&
-      selectedGeoJsonLayer.features.length > 0
-    ) {
-      // may be null on first render
-      // set features to map
-      const convertedFeatures = new GeoJSON().readFeatures(
-        selectedGeoJsonLayer
-      );
-
-      console.log(convertedFeatures);
-
-      featuresLayer.setSource(
-        new VectorSource({
-          features: convertedFeatures, // make sure features is an array
-          featureProjection: "EPSG:3857",
-        })
-      );
-
-      // fit map to feature extent (with 100px of padding)
-      map.getView().fit(featuresLayer.getSource().getExtent(), {
-        padding: [100, 100, 100, 100],
-      });
-    }
-  }, [features]);
-
-  // map click handler
-  const handleMapClick = (event) => {
-    // get clicked coordinate using mapRef to access current React state inside OpenLayers callback
-    //  https://stackoverflow.com/a/60643670
-    const clickedCoord = mapRef.current.getCoordinateFromPixel(event.pixel);
-
-    // transform coord to EPSG 4326 standard Lat Long
-    const transormedCoord = transform(clickedCoord, "EPSG:3857", "EPSG:4326");
-
-    // set React state
-    setSelectedCoord(transormedCoord);
-  };
-
-  // render component
   return (
     <div>
-      <div ref={mapElement} className="map-container"></div>
-
-      <div className="clicked-coord-label">
-        <p>{selectedCoord ? toStringXY(selectedCoord, 5) : ""}</p>
-      </div>
+      <Map center={fromLonLat(center)} zoom={zoom}>
+        <Layers>
+          <TileLayer source={osm()} zIndex={0} />
+          {selectedGeoJsonLayer && (
+            <VectorLayer
+              source={vector({
+                features: new GeoJSON().readFeatures(selectedGeoJsonLayer, {
+                  featureProjection: get("EPSG:3857"),
+                }),
+              })}
+              style={styles.MultiPolygon}
+            />
+          )}
+        </Layers>
+        <Controls>
+          <FullScreenControl />
+        </Controls>
+      </Map>
     </div>
   );
 }
